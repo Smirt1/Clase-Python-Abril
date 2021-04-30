d55f29af67da4a25476921bd9f2a296739ef8f22
Smirt Valentin <smirtvs@gmail.com>
2021-04-30 19:22:54 -0400

    first commit

---
+++ Dia1.py
@@ -0,0 +1,7 @@
+#Expresiones en python
+
+#entrada y salida de  datos en python
+
+dolar = input ("Ingrese cantidad de  dolares: ")
+pesos = int (dolar) * 58
+print ("El resultado es " + str (pesos))
