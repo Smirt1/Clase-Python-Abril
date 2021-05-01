68bbb98ca9cfa40ba23f2168fe9994b26e7dafa5
Smirt Valentin <smirtvs@gmail.com>
2021-05-01 08:06:24 -0400

    clase teminada

--- Dia1.py
+++ Dia1.py
@@ -2,6 +2,35 @@
 
 #entrada y salida de  datos en python
 
-dolar = input ("Ingrese cantidad de  dolares: ")
-pesos = int (dolar) * 58
-print ("El resultado es " + str (pesos))
+# dolar = input ("Ingrese cantidad de  dolares: ")
+# pesos = int (dolar) * 58
+# print ("El resultado es " + str (pesos))
+
+#Estructuras de control if / else / elif
+
+# edad =  int (input ("Ingrese su edad: "))
+# if edad >= 18:
+#     print ("Tenga su cerveza!!")
+#     print ("Salud!!")
+
+
+# compra = 2300
+
+# if compra >= 2500:
+#     print (compra * 0.05)
+# else:
+#     print ("No aplica para descuento")
+
+
+mes = 2
+if mes == 1:
+    print("Es enero")
+elif mes == 2:
+    print("Es febrero")
+    
+else:
+    print("No es un mes valido")
+
+
+
+#Bucles o ciclos
