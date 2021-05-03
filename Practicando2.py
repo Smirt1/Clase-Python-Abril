cb3383eb49b2c40f1139e2a73e3113617767b300
Smirt Valentin <smirtvs@gmail.com>
2021-05-03 09:53:33 -0400

    Mas cambios en practicando 2

---
+++ Practicando2.py
@@ -0,0 +1,61 @@
+# print("Hola! Dime tu nombre")
+# nombre = input()
+# print ("Hola, bienvenido")
+# print (nombre)
+
+# nombre = input ("Dime tu nombre: ")
+# print ("Hola, bienvenido: " + nombre)
+
+# numero_a = int (input ("Indroduzca el primer numero: "))
+# numero_b = int (input ("Indroduzca el segundo numero: "))
+# suma = (numero_a + numero_b)
+# print ("La Sumatoria es :" )
+# print (suma)
+
+# numero_a = int (input ("Indroduzca el primer numero: "))
+# numero_b = int (input ("Indroduzca el segundo numero: "))
+# resta = (numero_a - numero_b)
+# print ("La Resta es :" )
+# print (resta)
+
+# a = int (input ("Indroduzca el primer numero: "))
+# b = int (input ("Indroduzca el segundo numero: "))
+# c = int (input ("Indroduzca el tercer numero: "))
+# resultado1 = (a+b)*c
+# resultado2 = a*c+b*c
+# print (resultado1, resultado2 )
+
+# print("Dime el radio")
+# radio = int(input())
+# pi = 3.14
+# longitud = 2 * pi * radio
+# print ("Longitud de la circunferencia:")
+# print (longitud)
+
+# radio = float (input ("Dime el radio: "))
+# pi = 3.14
+# longitud = 2 * pi * radio
+# print ("Longitud de la circunferencia :", longitud )
+
+
+# a = float (input ("Indroduzca el primer numero: "))
+# b = float (input ("Indroduzca el segundo numero: "))
+# suma = (a + b)
+# print (suma)
+
+
+# a = int (input ("Indroduzca el primer numero: "))
+# b = int (input ("Indroduzca el segundo numero: "))
+# suma = (a + b)
+# print ("El resultado de la suma es: ", suma)
+
+# a = int (input ("Indroduzca el primer numero: "))
+# b = int (input ("Indroduzca el segundo numero: "))
+# resta = (a - b)
+# print ("El resultado de la resta es: ", resta)
+
+# a =  float (input ("Indroduzca el primer numero: "))
+# b =  float (input ("Indroduzca el segundo numero: "))
+# div = (a / b)
+# print ("El resultado de la division es: ", div)
+
