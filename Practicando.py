b5bd3c2fa6e882e1adcdb21fb85894825c593b65
Smirt Valentin <smirtvs@gmail.com>
2021-05-03 08:15:06 -0400

    Guardar practicando

---
+++ Practicando.py
@@ -0,0 +1,46 @@
+#Variables
+# cuenta = 75458
+# print (cuenta)
+
+# nombre_cliente = "John Snow"
+# condicion = True
+# print (type(condicion))
+# factura_numero = 123
+# x = 12.3
+
+#Convertir dolar a peso
+
+# cantidad_dolares = 89.75
+# tasa = 58.50
+# print (cantidad_dolares * tasa)
+
+# Con print / input
+
+# usuario = input("Escriba el nombre del usuario: ")
+# print ("Bienvenido : " + usuario)
+# cantidad_dolares =  float (input ("Ingrese la cantidad de dolares: "))
+# tasa = 58.50
+# print (cantidad_dolares * tasa)
+
+# usuario = input("Escriba el nombre del usuario: ")
+# print ("Bienvenido : " + usuario)
+
+#Operadores aritmeticos
+
+# print (10 % 6)
+# print (10 % 2)
+
+# print (1==2)
+# print (1 < 2)
+# print (2<=2)
+
+#Calendario
+
+import calendar
+year = 2021
+month = 5
+
+print (calendar.month(year, month))
+
+
+
