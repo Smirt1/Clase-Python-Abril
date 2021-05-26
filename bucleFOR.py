4c4cdbc05901d94ede5bd1957483ac6aa89880cc
Smirt Valentin <smirtvs@gmail.com>
2021-05-26 18:05:01 -0400

    tabla

---
+++ bucleFOR.py
@@ -0,0 +1,39 @@
+
+# #Lista
+
+
+# nombres = ["Juan", "Ana", "Elena","Smirt"]
+# print ("El tipo de nombre es", type (nombres))
+# for nombre in nombres:
+#     print ("Buenos dias", nombre)
+
+# #Tuple
+
+# nombres = ("Juan", "Ana", "Elena","Smirt")
+# print ("El tipo de nombre es", type (nombres))
+# for nombre in nombres:
+#     print ("Buenos dias", nombre)
+
+# #set
+
+# nombres = {"Juan", "Ana", "Elena","Smirt"}
+# print ("El tipo de nombre es", type (nombres))
+# for nombre in nombres:
+#     print ("Buenos dias", nombre)
+
+# #diccionario
+
+# nombres = {"Juan": 1, "Ana": 2, "Elena": 3,"Smirt": 4}
+# print ("El tipo de nombre es", type (nombres))
+# for nombre in nombres:
+#     print ("Buenos dias", nombre)
+
+numero = 5
+
+def mostrar_linea(num, linea):
+    print (num, "x", linea, "=", num * linea)
+
+for n in range (11):
+    mostrar_linea (numero, n)
+
+
---
+++ while.py
@@ -0,0 +1,15 @@
+
+# respuesta = int (input ("Cuantas lineas tiene jupiter? "))
+
+# while respuesta != 79:
+#     respuesta =  int (input ("Respuesta equivocada, trate de nuevo "))
+
+# print ("Acertaste!!!!!")
+
+
+pares = [2, 4, 6, 8, 10]
+
+i = 0
+while i < len (pares):
+    print (pares[i])
+    i += 1
